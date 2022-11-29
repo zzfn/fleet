2e181aa45f5df5d16548f9d4d8dab18aed424b9c
orluna <44751363+zzfn@users.noreply.github.com>
2022-11-29 17:42:20 +0800

    feat: array

--- src/components/NavCard.tsx
+++ src/components/NavCard.tsx
@@ -1,11 +1,10 @@
-import styles from "../../styles/Home.module.css";
 import Image from 'next/image'
 const NavCard = ({record}:any) => {
-  return <a rel="noreferrer" target='_blank' href={record.url} className={styles.card}>
+  return <a rel="noreferrer" target='_blank' href={record.url} className='shadow rounded flex flex-col justify-between items-center p-6'>
       <h2>{record.title} &rarr;</h2>
       <p>
           <Image
-              src="https://cdn.jsdelivr.net/gh/walkxhub/dashboard-icons/png/amazon.png"
+              src="https://cdn.orluma.ltd/midway/star.svg"
               alt={record.title}
               width={80}
               height={80}
